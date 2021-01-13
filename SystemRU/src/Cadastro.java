3e933e323242d26b20e23b2d82ad1426680562c3
Laura Lira <51386699+lauragabre@users.noreply.github.com>
2021-01-13 18:58:22 -0300

    Add files via upload

---
+++ SystemRU/src/Cadastro.java
@@ -0,0 +1,38 @@
+
+public class Cadastro {
+
+	private String nome;
+	private String cpf;
+	private String senha;
+
+	public Cadastro() {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
